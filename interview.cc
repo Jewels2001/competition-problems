d0da7bb73fd6f0049b356521370e2b12046a93e5
Julie Wojtiw-Quo <72999066+Jewels2001@users.noreply.github.com>
2021-11-23 16:20:12 -0700

    Add files via upload

--- interview.cc
+++ interview.cc
@@ -11,21 +11,45 @@ int main() {
   int ans = 0;
   cin >> c;
 
+  int highestA = a + (t/ta);
+  int highestB = b + (t/tb);
+
+  vector<pair<int, int>> v;
   priority_queue<pair<int, int>> p;
   //tech, busniess
 
   for(int i=1; i<=c; i++) {
     pair<int,int> t;
     cin >> t.first >> t.second;
-    p.push(t);
+    if((t.first <= a) && (t.second <= b)) {
+      ans++;
+    }
+    else {
+      v.push_back(t);
+    }
   }
-  //cout:
-  cout << " ";
-  //cin:
-  cin >> "";
 
-  //round up
-  (A+(B-1)) /B
+  sort(begin(v), end(v));
 
+  for(int i=0; i<v.size();i++) {
+    int needA = v[i].first - a;
+    int needB = v[i].second - b;
+    if(((v[i].first > highestA) || (v[i].second > highestB) {
+      //(v[i].first - a) > t/ta
+      continue;
+    }
+
+    else if ((needA*ta + needB*tb) > t) {
+      continue;
+    }
+    int newA = a + needA;
+    int newB = b + needB;
+    cout << "newa: " << newA << "newb: " << newB << endl;
+    cout << "ans: " << ans << endl;
+    ans++;
+
+  }
+
+  cout << ans << endl;
   return 0;
 }
